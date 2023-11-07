ba5b80cd3912fd70c3b951a45207895e52e08fe0
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-07 12:08:41 -0500

    :sparkles: create example for do dictionary with Dictionary  Comprehesion

--- 05_dict.py
+++ 05_dict.py
@@ -1,2 +1,16 @@
+import random
+
 exponents = {i: i ** 2 for i in range(1, 11)}
 print(exponents)
+
+countries = ["col", "mex", "bol", "pe"]
+
+population = {country: random.randint(1, 100) for country in countries}
+print(population)
+
+population = {country: population for (country, population) in population.items() if population > 50}
+print(population)
+
+text = "Hola, soy Ernesto"
+vowels = {c: c.upper() for c in text if c in "aeiou"}
+print(vowels)
